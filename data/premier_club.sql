490a193546a87e808547441f956c4b1dee7f3855
Oleg Motruk Home <olegmotruk94@gmail.com>
2025-03-02 11:04:31 +0200

    data added

---
+++ data/premier_club.sql
@@ -0,0 +1,37 @@
+-- MySQL dump 10.13  Distrib 8.0.40, for Win64 (x86_64)
+--
+-- Host: localhost    Database: premier
+-- ------------------------------------------------------
+-- Server version	8.0.40
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Dumping data for table `club`
+--
+
+LOCK TABLES `club` WRITE;
+/*!40000 ALTER TABLE `club` DISABLE KEYS */;
+INSERT INTO `club` VALUES (1,'Manchester United','Manchester','Ruben Amorim','England',1878,NULL,1),(2,'LiverpoolFC','Liverpool','Arne Slot','England',1892,NULL,2),(3,'Manchester City','Manchester','Pep Guardiola','England',1880,NULL,3),(4,'Chelsea','London','Enzo Mareska','England',1905,NULL,4),(5,'Tottenham','London','Ange Postekoglu','England',1882,NULL,5),(6,'Arsenal','London','Mikel Arteta','England',1886,NULL,6),(7,'New Castle','Newcastle upon Tyne','Eddie Hau','England',1881,NULL,7),(8,'Aston Villa','Birmingham','Unai Emery','England',1874,NULL,8),(9,'Everton','Liverpool','David Moyes','England',1878,NULL,9),(10,'West Ham','London','Grem Potter','England',1895,NULL,10);
+/*!40000 ALTER TABLE `club` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2025-03-02 11:03:37
